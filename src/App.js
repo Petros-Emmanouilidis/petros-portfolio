0c8febf1969666a8f76eca84564cf1feefd98fe5
Petros-Emmanouilidis <pemmanou@andrew.cmu.edu>
2024-07-03 14:17:55 +0300

    I guess this works?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, OrbitControls } from "@react-three/drei";
 
-function App() {
+
+import { Armchair } from './Armchair'
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <Canvas camera={{ fov: 70, position: [0, 2, 20] }}>
+        <Suspense fallback={null}>
 
-export default App;
+          <Armchair/>
+          <Armchair position={[-10, 0 , 0]}/>
+          <Armchair position={[10, 0 , 0]}/>
+
+          <OrbitControls />
+          <Environment preset="sunset" background backgroundBlurriness={1}/>
+        </Suspense>
+      </Canvas>
+    </div>
+  )
+}
